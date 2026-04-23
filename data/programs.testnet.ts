d1def3786df64934503019562d2522620608a971
Michael Ghen <mike@mikeghen.com>
2026-04-23 09:30:01 -0400

    chore: add new V2/V3 capped minters (#30)

--- data/programs.testnet.ts
+++ data/programs.testnet.ts
@@ -24,6 +24,48 @@ export const TESTNET_PROGRAMS: ProgramConfig[] = [
     rootAddresses: ["0xb8018036649b88bb3ee74a46d87a940d7b39076a"],
     chainId: 300,
   },
+  {
+    name: "4 Mod",
+    proposalUrl: "https://sepolia.explorer.zksync.io",
+    rootAddresses: ["0xCab3350EC4aF384901395561E9aFD8E6D052c6bb"],
+    chainId: 300,
+  },
+  {
+    name: "5 Mod",
+    proposalUrl: "https://sepolia.explorer.zksync.io",
+    rootAddresses: ["0x55E2EFAe8641153d2979332b29380FDE34Ce0a44"],
+    chainId: 300,
+  },
+  {
+    name: "6 Mod",
+    proposalUrl: "https://sepolia.explorer.zksync.io",
+    rootAddresses: ["0x7350Be6Ed47cA21075f098422e47375cC49fd1Ba"],
+    chainId: 300,
+  },
+  {
+    name: "7 Mod",
+    proposalUrl: "https://sepolia.explorer.zksync.io",
+    rootAddresses: ["0xa966432eF4c6C027f7Acbd710e070Cdb652D45Be"],
+    chainId: 300,
+  },
+  {
+    name: "8 Mod",
+    proposalUrl: "https://sepolia.explorer.zksync.io",
+    rootAddresses: ["0x620Bf4BbEb0C31D221Ab674587A6d02E85636B8C"],
+    chainId: 300,
+  },
+  {
+    name: "9 Mod",
+    proposalUrl: "https://sepolia.explorer.zksync.io",
+    rootAddresses: ["0xD02dc616e42230597443DE81E03513879c2b1654"],
+    chainId: 300,
+  },
+  {
+    name: "10 Mod",
+    proposalUrl: "https://sepolia.explorer.zksync.io",
+    rootAddresses: ["0x1181F23559aF97851e2834c888373Cd2baF5dD6e"],
+    chainId: 300,
+  }
 ];
 
 export const TESTNET_PROGRAM_ROOTS: {
